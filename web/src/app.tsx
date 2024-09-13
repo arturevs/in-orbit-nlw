666f69c0a965ed78f04ee79a43a6d49f17096e5b
arturevs <artur.rp2001@gmail.com>
2024-09-12 22:16:15 -0300

    app change

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,31 +2,21 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { Summary } from './components/summary'
 import { EmptyGoals } from './components/empty-goals'
-import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
 
 export function App() {
-  const [count, setCount] = useState(5)
-
-  function increment() {
-    setCount(count + 1)
-  }
-
-  fetch('http://localhost:3333/summary')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      console.log(data)
-    })
+  const { data } = useQuery({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+  })
 
   return (
     <Dialog>
-      <button type="button" onClick={increment}>
-        Incrementar
-      </button>
-      <h1 className="text-4xl">{count}</h1>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
+      {/*  */}
+      {/*  */}
       <CreateGoal />
     </Dialog>
   )
